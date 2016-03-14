b23e48c71097b71395794fd41641b8df79e79172
Alanyeh <alanyeh20001@hotmail.com>
2016-03-14 15:37:10 +0800

    modify capistrano setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-
+server '52.192.186.143', :app, :web, :db, :primary => true
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
@@ -17,9 +17,9 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w{deploy@52.192.186.143}
-role :web, %w{deploy@52.192.186.143}
-role :db,  %w{deploy@52.192.186.143}
+#role :app, %w{deploy@example.com}, my_property: :my_value
+# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
+# role :db,  %w{deploy@example.com}
 
 
 
